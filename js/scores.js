05138df4f1a9c65daff4e1873198812db43bfa7e
Marcel Njighe <marceltezeh677@gmail.com>
2020-11-27 10:40:32 -0500

    updated the server

--- js/scores.js
+++
@@ -1,9 +0,0 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-// display the list of scores from local Storage which is an array
-highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} = ${score.score}</li>`;
-  })
-  .join("");
